038f1b7cfa3e613888065ca57a83a85185537c29
Evan Young <cantis@gmail.com>
2025-02-04 21:44:01 -0600

    Add time entry defaults and enhance form with duration calculation

--- app/app.py
+++ app/app.py
@@ -16,6 +16,10 @@ def create_app() -> Flask:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+    # Load start and end times
+    app.config['DAY_START_TIME'] = int(os.getenv('DAY_START_TIME', '08:00').split(':')[0]) * 60 + int(os.getenv('DAY_START_TIME', '08:00').split(':')[1])
+    app.config['DAY_END_TIME'] = int(os.getenv('DAY_END_TIME', '17:00').split(':')[0]) * 60 + int(os.getenv('DAY_END_TIME', '17:00').split(':')[1])
+
     db.init_app(app)
     with app.app_context():
         db.create_all()
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-
-from flask import Blueprint, redirect, render_template, request, url_for
+from datetime import datetime, timedelta
+from flask import Blueprint, current_app, redirect, render_template, request, url_for
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, StringField, SubmitField
 from wtforms.validators import DataRequired, Optional
@@ -38,16 +37,32 @@ def add_entry() -> str:
         (use.id, use.description) for use in CommonTimeUse.query.order_by(CommonTimeUse.description).all()
     ]
 
-    if form.validate_on_submit():
-        entry = TimeEntry(
-            date=datetime.strptime(form.date.data, '%Y-%m-%d'),
-            start_time=form.start_time.data,
-            duration=form.duration.data,
-            description=form.description.data,
-            common_use_id=form.common_use.data if form.common_use.data != 0 else None,
-        )
-        db.session.add(entry)
-        db.session.commit()
-        return redirect(url_for('home.index'))
+    # Default the date entry box to today's date
+    if request.method == 'GET':
+        form.date.data = datetime.now().strftime('%Y-%m-%d')
+
+        # Default the start time to be the end of the most recent entry for today
+        today = datetime.now().strftime('%Y-%m-%d')
+        latest_entry = TimeEntry.query.filter(TimeEntry.date.cast(db.Date) == today).order_by(TimeEntry.start_time.desc()).first()
+        if latest_entry:
+            form.start_time.data = latest_entry.start_time + latest_entry.duration
+        else:
+            form.start_time.data = current_app.config['DAY_START_TIME']
+
+    try:
+        if form.validate_on_submit():
+            entry = TimeEntry(
+                date=datetime.strptime(form.date.data, '%Y-%m-%d'),
+                start_time=form.start_time.data,
+                duration=form.duration.data,
+                description=form.description.data,
+                common_use_id=form.common_use.data if form.common_use.data != 0 else None,
+            )
+            db.session.add(entry)
+            db.session.commit()
+            return redirect(url_for('home.index'))
+    except Exception as e:
+        db.session.rollback()
+        return render_template('home/add_entry.html', form=form, error=str(e))
 
     return render_template('home/add_entry.html', form=form)
